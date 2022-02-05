21989d905817c2893c125af64d988ad1ac933510
Nil Vila <nilvilamar@gmail.com>
2022-02-05 12:26:31 +0100

    Removed global data fetch

--- pages/_document.js
+++ pages/_document.js
@@ -29,7 +29,7 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <Layout />
+          <Main />
           <NextScript />
         </body>
       </Html>
