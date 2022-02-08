15a26bc5956e19af7f718fd6a27a8d820cf88ea8
Nil Vila <nilvilamar@gmail.com>
2022-02-08 18:10:02 +0100

    Updated tailwind custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,89 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      white: '#FDFEFF',
+      black: '#16171D',
+      gray: {
+        10: '#F6F7FA',
+        20: '#F0F1F5',
+        30: '#E9EAF0',
+        40: '#DADDE8',
+        50: '#CBCDDA',
+        60: '#AAAEBF',
+        70: '#888C9C',
+        80: '#686B79',
+        90: '#4A4C59',
+        100: '#31333D',
+      },
+      brand: {
+        10: '#EEEEFD',
+        20: '#CFCCF9',
+        30: '#9787F5',
+        40: '#6E35E7',
+        50: '#42278D',
+        60: '#2C1E50',
+      },
+    },
+    fontFamily: {
+      sans: ['GT Cinetype', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Helvetica Neue', 'Helvetica', 'Ubuntu', 'Roboto', 'Noto', 'Arial', 'sans-serif'],
+    },
+    fontSize: {
+      '7xl': ['5.25rem', {
+        letterSpacing: '-0.04em',
+        lineHeight: '1',
+      }],
+      '6xl': ['4.4rem', {
+        letterSpacing: '-0.04em',
+        lineHeight: '1',
+      }],
+      '5xl': ['3.65rem', {
+        letterSpacing: '-0.04em',
+        lineHeight: '1.1',
+      }],
+      '4xl': ['3.05rem', {
+        letterSpacing: '-0.03em',
+        lineHeight: ' 1.1',
+      }],
+      '3xl': ['2.1rem', {
+        letterSpacing: '-0.02em',
+        lineHeight: '1.1',
+      }],
+      '2xl': ['1.75rem', {
+        letterSpacing: '-0.02em',
+        lineHeight: '1.2',
+      }],
+      xl: ['1.45rem', {
+        letterSpacing: '-0.02em',
+        lineHeight: '1.25',
+      }],
+      l: ['1.2rem', {
+        letterSpacing: '-0.01em',
+        lineHeight: '1.3',
+      }],
+      m: ['1rem', {
+        letterSpacing: '-0.01em',
+        lineHeight: '1.4',
+      }],
+      s: ['0.85rem', {
+        letterSpacing: '-0.01em',
+        lineHeight: '1.4',
+      }],
+      xs: ['0.7rem', {
+        letterSpacing: '0em',
+        lineHeight: '1.4',
+      }],
+    },
+    extend: {
+      spacing: {
+        xs: '16px',
+        s: '32px',
+        m: '48px',
+        l: '80px',
+        xl: '128px',
+        '2xl': '208px',
+      },
+    },
   },
   plugins: [],
 };
